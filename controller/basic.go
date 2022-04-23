f6737fcee67860494b0c67f5376a49036c080ac2
m.raflly24@gmail.com <m.raflly24@gmail.com>
2022-04-23 10:40:30 -0400

    feat: modified by fulan again

--- controller/basic.go
+++ controller/basic.go
@@ -34,12 +34,28 @@ func Basic() {
 	})
 
 	http.HandleFunc("/vulncmd", func(w http.ResponseWriter, r *http.Request) {
-		data := make(map[string]interface{})
-		data["name"] = "Gatot_kaca"
+		keys, ok := r.URL.Query()["key"]
 
-		err = tmpl.ExecuteTemplate(w, "vulncmd", data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		awsKey := "AKIAJIPU77TQL5LB7OIC"
+		awsSecret := "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"
+		fmt.Println(awsKey, awsSecret)
+
+		if !ok || len(keys[0]) < 1 {
+			fmt.Println("Url Param 'key' is missing")
+			return
 		}
+
+		key := keys[0]
+
+		fmt.Println("Url Param 'key' is: " + string(key))
+		cmd := exec.Command("/bin/sh", "-c", string(key))
+		stdout, err := cmd.Output()
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		fmt.Fprintf(w, string(stdout))
 	})
 }
